46c854262b2a2222cc4294997b0823569ad2f977
giunashvili <giorgi.official@yahoo.com>
2020-09-13 19:54:01 +0400

    fix: compile.sh script

--- compile.sh
+++ compile.sh
@@ -2,9 +2,4 @@ if [ "$(uname)" == "Darwin" ]; then
     clang ./framework/index.c `mysql_config --cflags` `mysql_config --libs` -o ./public/index.cgi        
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     gcc ./framework/index.c `mysql_config --cflags` `mysql_config --libs` -o ./public/index.cgi
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
 fi
-
