9fdcf1b2d978edae4331bcd0a0856a0eb37a37de
giunashvili <giorgi.official@yahoo.com>
2020-09-13 19:51:44 +0400

    add: compiling bash script for linux

--- compile.sh
+++ compile.sh
@@ -1,1 +1,10 @@
-clang ./framework/index.c `mysql_config --cflags` `mysql_config --libs` -o ./public/index.cgi
+if [ "$(uname)" == "Darwin" ]; then
+    clang ./framework/index.c `mysql_config --cflags` `mysql_config --libs` -o ./public/index.cgi        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    gcc ./framework/index.c `mysql_config --cflags` `mysql_config --libs` -o ./public/index.cgi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
+
